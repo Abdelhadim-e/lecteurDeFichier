1b7fd5a8f251ed3d79728fb7b889eb8a1fde86a9
Abdelhadim <e.abdelhadim@gmail.com>
2019-11-11 11:29:19 +0100

    Ajout fichier LecteurMain, affichage lecteur

---
+++ src/lecteurdefichier/LecteurMain.java
@@ -0,0 +1,12 @@
+package lecteurdefichier;
+
+public class LecteurMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Affichage lecteur");
+		
+
+	}
+
+}
